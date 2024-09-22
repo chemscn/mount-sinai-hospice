0ff13bd62ece6a031b2becd973c145848415fd9b
Diyar Karim <dkarim@softerware.com>
2024-09-22 13:32:31 -0500

    Another update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
-});
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
-});
 
 export const metadata: Metadata = {
 	title: 'Mount Sinai Hospice',
@@ -31,7 +20,7 @@ export default function RootLayout({
 			lang="en"
 		>
 			<body
-				className={`flex flex-col min-h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
+				className="flex flex-col min-h-screen"
 			>
 				<Navbar />
 				<main className="flex-grow">{children}</main>
