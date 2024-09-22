9689ca49fc9b26fbba6e9555ef084dbb16e4f13f
Diyar Karim <dkarim@softerware.com>
2024-09-22 12:17:19 -0500

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
+	images: {
+		remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+                port: '',
+                pathname: '**'
+            }
+        ]
+	}
 };
 
 export default nextConfig;
